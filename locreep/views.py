4a70c7fa963a4e48ad9a909cb04681a586a59650
ffmaer <tz348@nyu.edu>
2011-10-02 03:01:37 -0400

    I added CSRF
    Signed-off-by: ffmaer <tz348@nyu.edu>

--- locreep/views.py
+++ locreep/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from twilio.rest import TwilioRestClient
 from twilio import twiml
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
 
 account = "ACb77594eb2632a2d77422086328ef03a9"
 token = "536e78251ae04f88ce7828ecd66fc673"
@@ -29,7 +32,8 @@ def xml(request):
     r.say("Hello")
     return HttpResponse(str(r),content_type="application/xhtml+xml")
 
-
+@csrf_exempt
+@require_POST
 def myGroups(request):
 	
     # call = client.calls.create(to="+19178551541", from_="+13475148471", url="http://foo.com/call.xml")
