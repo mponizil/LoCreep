43185d345c58b9ad0abd5407db58ddc3b7c2cffa
Randall Hunt <randallhunt@gmail.com>
2011-10-02 02:53:52 -0400

    edited models some

--- locreep/models.py
+++ locreep/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.CharField(max_length = 60)
-    password = models.CharField(max_length = 60)
-    fname = models.CharField(max_length = 20)
-    lname = models.CharField(max_length = 20)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
 
     def __unicode__(self):
         return self.fname
 
 class Group(models.Model):
-    name = models.CharField(max_length = 30)
-    phone = models.IntegerField(max_length = 10)
+    name = models.CharField(max_length=30)
+    phone = models.IntegerField(max_length=10)
     description = models.TextField()
 
     def __unicode__(self):
@@ -26,9 +26,9 @@ class UserInGroups(models.Model):
         return self.id
 
 class Creep(models.Model):
-    phone = models.IntegerField(max_length = 10)
-    photo = models.CharField(max_length = 75)
-    last_seen = models.CharField(max_length = 255)
+    phone = models.IntegerField(max_length=10)
+    photo = models.CharField(max_length=75)
+    last_seen = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.phone
