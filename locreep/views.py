a24aa16bebece48595372ac30a2d16d2fa9b65d3
Misha Ponizil <misha.ponizil@gmail.com>
2011-10-01 21:57:41 -0400

    gotta see what data twilio posts to us

--- locreep/views.py
+++ locreep/views.py
@@ -1,6 +1,11 @@
 from locreep import models
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from twilio.rest import TwilioRestClient
+
+account = "ACb77594eb2632a2d77422086328ef03a9"
+token = "536e78251ae04f88ce7828ecd66fc673"
+client = TwilioRestClient(account, token)
 
 def welcome(request):
     return HttpResponse("wassup")
@@ -9,4 +14,8 @@ def phone(request):
     return HttpResponse("phone")
 
 def text(request):
-    return HttpResponse("text")
+    print request
+    # message = client.sms.messages.create(to="+17608463179",
+    #                                      from_="+13475148471",
+    #                                      body="Hello!")
+    return HttpResponse(request)
--- urls.py
+++ urls.py
@@ -3,6 +3,11 @@ from lc.locreep import views
 
 urlpatterns = patterns('',
 	(r'^$', views.welcome),
+	
 	(r'^phone/$', views.phone),
 	(r'^text/$', views.text),
+	
+	#(r'^search/$', views.search),
+	#(r'^groups/$', views.groups),
+	#(r'^creeps/$', views.creeps),
 )
