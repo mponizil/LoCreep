221550a5a960bb7b0fba658b02f73d0679445778
Randall <randallhunt@gmail.com>
2011-10-02 02:16:03 +0000

    updated settings

--- urls.py
+++ urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls.defaults import *
-from lc.locreep import views
+from locreep import views
 
 urlpatterns = patterns('',
 	(r'^$', views.welcome),
-	
 	(r'^phone/$', views.phone),
 	(r'^text/$', views.text),
-	
+
 	#(r'^search/$', views.search),
 	#(r'^groups/$', views.groups),
 	#(r'^creeps/$', views.creeps),
