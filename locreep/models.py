cc403af7e4d5b5d752d2fd90ecbe58badc2caa4b
Randall Hunt <randallhunt@gmail.com>
2011-10-02 04:51:27 -0400

    models update

--- locreep/models.py
+++ locreep/models.py
@@ -13,40 +13,34 @@ class Group(models.Model):
     name = models.CharField(max_length=30)
     phone = models.IntegerField(max_length=10)
     description = models.TextField()
+    creeps = models.ManyToManyField(Creep, verbose_name="creeps in a group")
+    users = models.ManyToManyField(User, verbose_name="users in a group") 
 
     def __unicode__(self):
         return self.name
 
-class UserInGroups(models.Model):
-    user_id = models.ForeignKey(User)
-    group_id = models.ForeignKey(Group)
-    confirmed = models.BooleanField()
-
-    def __unicode__(self):
-        return self.id
-
 class Creep(models.Model):
     phone = models.IntegerField(max_length=10)
     photo = models.CharField(max_length=75)
-    last_seen = models.CharField(max_length=255)
+    name = models.CharField(max_length=200)
 
     def __unicode__(self):
         return self.phone
 
 class Conversation(models.Model):
-    group_id = models.ForeignKey(Group)
-    creep_id = models.ForeignKey(Creep)
-    tumblr_id = models.CharField(max_length = 255)
-    qr_id = models.CharField(max_length = 255)
+    group = models.ForeignKey(Group, verbose_name="group this conversation belongs to")
+    creep = models.ForeignKey(Creep, verbose_name="creep this conversation belongs to")
+    tumblr_id = models.CharField(max_length=255)
+    qr_id = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.group_id
 
 class Message(models.Model):
     conversation_id = models.ForeignKey(Conversation)
-    user_type = models.CharField(max_length = 10)
-    user_id = models.ForeignKey(User)
-    creep_id = models.ForeignKey(Creep)
+    user_type = models.CharField(max_length=10)
+    user = models.ForeignKey(User)
+    creep = models.ForeignKey(Creep)
 
     def __unicode__(self):
-        return self.phone
+        return self.conversation_id
