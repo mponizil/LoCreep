c464cc19c8a5e94bc6a359fa228bb7fffa4a527b
Misha Ponizil <misha.ponizil@gmail.com>
2011-10-29 15:38:42 -0400

    some twilio stuff

--- locreep/twil.py
+++ locreep/twil.py
@@ -13,7 +13,7 @@ from django.views.decorators.http import require_POST
 import urllib
 import urllib2
 
-domain_name = "http://3iwj.localtunnel.com"
+domain_name = "http://3q6q.localtunnel.com"
 
 account = "ACb77594eb2632a2d77422086328ef03a9"
 token = "536e78251ae04f88ce7828ecd66fc673"
