14d15b20abf7f95b3b3a65f26390b7359e600d88
Misha Ponizil <misha.ponizil@gmail.com>
2011-11-22 20:27:26 -0500

    prepared to send mass email

--- locreep/management/commands/valid.py
+++ locreep/management/commands/valid.py
@@ -5,15 +5,16 @@ from django.contrib.auth.models import User
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from datetime import datetime
 
 import random, string
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
-        users = User.objects.all()[0:200]
+        users = User.objects.filter(date_joined__lte=datetime(2011,11,17))
 
         for user in users:
-            if not user.email:
+            if not user.email or not re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$",user.email):
                 # user.delete()
                 print 'deleting user without email\n'
             else:
@@ -40,8 +41,8 @@ class Command(BaseCommand):
 
                 link = "http://locreep.com/valid-email/" + rand_str
 
-                text = "Hello LoCreeper,\n\nThanks for joining LoCreep, we are happy to have you on board! There are new features brewing, so stick around as we assemble the world's first Creeper prevention software.\n\nBefore becoming a card-carrying LoCreep user, we need you to validate your email in order to use the tools and receive a LoCreep phone number. Please click the following link to validate your email address. Thank you!\n\n" + link + "\n\nHappy creeping!\nThe LoCreep Team"
-                html = "<html><head></head><body>Hello LoCreeper,<br /><br />Thanks for joining LoCreep, we are happy to have you on board! There are new features brewing, so stick around as we assemble the world's first Creeper prevention software.<br /><br />Before becoming a card-carrying LoCreep user, we need you to validate your email in order to use the tools and receive a LoCreep phone number. Please click the following link to validate your email address. Thank you!<br /><br /><a href='" + link + "'>" + link + "</a><br /><br />Happy creeping!<br />The LoCreep Team</body></html>"
+                text = "Hello LoCreeper,\n\nThanks for joining LoCreep, we are happy to have you on board! There are new features brewing, so stick around as we assemble the world's first Creeper prevention software.\n\nBefore becoming a card-carrying LoCreep user, we need you to verify your email in order to continue using the tools and keep your LoCreep phone number. Please click the following link to verify your email address. Thank you!\n\n" + link + "\n\nHappy creeping!\nThe LoCreep Team"
+                html = "<html><head></head><body>Hello LoCreeper,<br /><br />Thanks for joining LoCreep, we are happy to have you on board! There are new features brewing, so stick around as we assemble the world's first Creeper prevention software.<br /><br />Before becoming a card-carrying LoCreep user, we need you to verify your email in order to continue using the tools and keep your LoCreep phone number. Please click the following link to verify your email address. Thank you!<br /><br /><a href='" + link + "'>" + link + "</a><br /><br />Happy creeping!<br />The LoCreep Team</body></html>"
 
                 part1 = MIMEText(text, 'plain')
                 part2 = MIMEText(html, 'html')
